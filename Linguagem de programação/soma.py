bb6b926e7fd02d7a2e4745f22ffbfedc74cef8d7
Cinthyamartins <cinthyacursotecnico@gmail.com>
2024-05-06 18:49:12 -0300

    exercicio

--- Linguagem de programação/soma.py
+++
@@ -1,7 +0,0 @@
-print("Calculadora soma")
-print("Digite nota soma")
-entrada1 = int(input())
-print("Digite nota 2")
-entrada2 = int(input())
-soma = entrada1 + entrada2
-print(soma)
--- Linguagem de programação/soma2.py
+++
@@ -1,8 +0,0 @@
-numero = 42
-
-chute = int(input ("Digite um numero:"))
-if chute == numero:
-    print("Você Acertou")
-else:
-    print("Você errou")
-
